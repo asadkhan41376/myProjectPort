2b14e7743d2d9a104929df56a2af3f54a75da8bc
Mohammed-Saif-Khan <mdsaif4449@gmail.com>
2024-02-12 21:35:55 +0530

    Add some changes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import "@/app/about/about.scss";
 import Button from "@/components/Button";
@@ -6,7 +7,6 @@ import { FaInstagram } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
 import Link from "next/link";
-
 
 function page() {
   useEffect(() => {
@@ -30,14 +30,14 @@ function page() {
       <div className="About-container">
         <div className="overaly"></div>
         <div className="containt">
-          <h3>I'm a Designer</h3>
+          <h3>I&apos;m a Designer</h3>
           <h1>
             I Can Design Anything <br /> You Want
           </h1>
           <p>
-            Hello there! I'm a web designer, and I'm very passionate and
-            dedicated to my work. With 20 years experience as a professional web
-            developer, I have acquired the skills and knowledge necessary to
+            Hello there! I&apos;m a web designer, and I&apos;m very passionate
+            and dedicated to my work. With 20 years experience as a professional
+            web developer, I have acquired the skills and knowledge necessary to
             make your project a success. I enjoy every step of the design
             process, from discussion and collaboration.
           </p>
@@ -45,24 +45,26 @@ function page() {
         </div>
       </div>
       <div className="section-2">
-        <img src="/dot.png"  className="dotImag"alt="" />
+        <img src="/dot.png" className="dotImag" alt="" />
         <h1>
-         <span className="h1-span"> Get in touch </span>  with me on social media. It's the best way to stay
-          connected with me and get quick updates on what I'm up to. Don't miss
-          out on this opportunity to connect, so follow me on your preferred
-          social media platform today!
+          <span className="h1-span"> Get in touch </span> with me on social
+          media. It&apos;s the best way to stay connected with me and get quick
+          updates on what I&apos;m up to. Don&apos;t miss out on this
+          opportunity to connect, so follow me on your preferred social media
+          platform today!
         </h1>
         <div className="icons-div">
-         
           <div className="elm" data-img="/insta.avif">
             <FaInstagram className="icon instagram" href="" />
           </div>
           <div className="elm" data-img="/yt.avif">
-          <Link href="">  <FaYoutube className="icon yt" /></Link>
+            <Link href="">
+              {" "}
+              <FaYoutube className="icon yt" />
+            </Link>
           </div>
           <div className="elm" data-img="/wb.avif">
-          <FaWhatsapp  className="icon whatsup"/>
-
+            <FaWhatsapp className="icon whatsup" />
           </div>
         </div>
       </div>
