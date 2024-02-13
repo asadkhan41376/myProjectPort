8dcbc44bb805740bb19466477b13b438d7fc2bbf
mohammad asad <asadkhan41376@gmail.com>
2024-02-13 14:03:44 +0530

    allowFullScreen

--- src/app/contect/page.js
+++ src/app/contect/page.js
@@ -14,7 +14,7 @@ function page() {
         <div className="map">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3628.016054238719!2d73.65897897405762!3d24.58864215608291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3967e549a6cf8489%3A0x885fcbc701684750!2sMastan%20Baba%20Dargah%20and%20Masjid!5e0!3m2!1sen!2sin!4v1707549806242!5m2!1sen!2sin"
-            allowfullscreen=""
+            allowFullScreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
