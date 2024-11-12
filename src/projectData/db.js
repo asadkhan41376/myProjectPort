2b09090491d42eda33a5523c5e4f4014f0ce8565
mohammad asad <asadkhan41376@gmail.com>
2024-11-12 17:29:23 +0530

    one new project add

--- src/projectData/db.js
+++ src/projectData/db.js
@@ -1,11 +1,19 @@
 export  const data = [
    
 
-
   {
 
     ImgSrc:
-    "/img/card.mp4",
+    "/video/flipcart.mp4",
+    title: "Clone FlipCart",
+    Text: "Welcome to the Clone FlipCart , In this project, we'll create a minimalist and stylish clone Other website using the power of Next.js, Mui, redux-tool-kit In This project will include a user login feature, integrated with the API from dummyjson.com.  we’ll implement a functional shopping cart with add-to-cart capabilities, so users can easily add items to their cart.these features will provide a realistic and interactive e-commerce experience. ",
+    links:"https://clone-flip-cart.vercel.app/",
+    category:"Next_React"
+  },
+  {
+
+    ImgSrc:
+    "/video/card.mp4",
     title: "Profile Card",
     Text: "Welcome to the Simple Profile Card project, In this project, we'll create a minimalist and stylish profile card using the power of HTML, CSS, and JavaScript. The profile card will showcase basic information about an individual, including their name, role, and location. ",
     links:"https://asadkhan41376.github.io/my-first-website",
@@ -14,7 +22,7 @@ export  const data = [
   {
 
     ImgSrc:
-    "/img/quiz.mp4",
+    "/video/quiz.mp4",
     title: " Quiz App ",
     Text: "Welcome to the Simple Quiz App project, In this project, we'll create a minimalist and stylish Quiz App using the power of HTML, CSS, and JavaScript. The Quiz App will showcase basic Quiz four quistions and answer or result pf the quiz. ",
     links:"https://webfunquiz.netlify.app/",
@@ -24,7 +32,7 @@ export  const data = [
   {
 
     ImgSrc:
-    "/img/buzy.mp4",
+    "/video/buzy.mp4",
     title: "Buzynet-site",
     Text: "Welcome to the Tour Website In this project, we'll create a minimalist and stylish Buzynet Website using the power of HTML, CSS, JavaScript. and bootsrap ",
     links:"https://deft-bunny-468651.netlify.app",
@@ -33,7 +41,7 @@ export  const data = [
     {
      
       ImgSrc:
-      "/img/tour.mp4",
+      "/video/tour.mp4",
       title: "Tour Website ",
       Text: "Welcome to the Tour Website In this project, we'll create a minimalist and stylish Tour Website using the power of HTML, CSS, JavaScript. and bootsrap ",
       links:"https://vocal-stroopwafel-fda415.netlify.app",
@@ -42,7 +50,7 @@ export  const data = [
     {
      
       ImgSrc:
-      "/img/weather.mp4",
+      "/video/weather.mp4",
       title: "Weather App",
       Text: "Welcome to the Simple weather app, In this project, we'll create a minimalist and stylish simple weather app using the power of next js., api and mui ",
       links:"https://weather-app-five-iota-35.vercel.app/",
