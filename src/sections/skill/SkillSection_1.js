8a772cde1ae7405fcf32d5d7638dcab6df05a16d
mohammad asad <asadkhan41376@gmail.com>
2024-11-22 10:25:45 +0530

    some change in skill page

--- src/sections/skill/SkillSection_1.js
+++ src/sections/skill/SkillSection_1.js
@@ -1,67 +1,66 @@
 "use client"
 import React, { useRef } from 'react'
 import { motion } from "framer-motion"
-import { Scale } from '@mui/icons-material'
 import Image from 'next/image'
 
 
-const ImgData =[
+const ImgData = [
     {
-        title:"HTML",
-        IMg:"/img/icons8-html.webp"
+        title: "HTML",
+        IMg: "/img/icons8-html.webp"
     },
     {
-        title:" CSS",
-        IMg:"/img/css.webp"
+        title: " CSS",
+        IMg: "/img/css.webp"
     },
     {
-        title:"JAVA SCRIPT",
-        IMg:"/img/icons8-js-300.webp"
+        title: "JAVA SCRIPT",
+        IMg: "/img/icons8-js-300.webp"
     },
     {
-        title:"BOOTSTRAP",
-        IMg:"/img/bootstrap.webp"
+        title: "BOOTSTRAP",
+        IMg: "/img/bootstrap.webp"
     },
     {
-        title:"NEXT JS",
-        IMg:"/img/icons8-nextjs-300.webp"
+        title: "NEXT JS",
+        IMg: "/img/icons8-nextjs-300.webp"
     },
 ]
 
 
 function SkillSection_1() {
-    const ref = useRef(null)
-  return (
-    <>
-<div className="Skill"  ref={ref}>
-    <h1 className='Skill_title'>skills</h1> 
 
-    <div className="perent ">
+    return (
+        <>
+            <div className="Skill" >
+                <h1 className='Skill_title'>skills</h1>
 
-{
-    ImgData.map((e,key)=>{
-        return(
-            <>
-            <motion.div drag  dragConstraints={ref} 
-            whileDrag={{scale:1.1}}
-            
-            className="Skill_Card" >
-          
-            <Image  fill={true} quality={100}  src={e.IMg} alt="skill img" />
-             <div className='overLay'><h1>{e.title}</h1></div>   
-        
-           </motion.div>
-           </>
-        )
-    })
-}
-    </div>
-   
+                <div className="perent ">
 
- 
-</div>
-    </>
-  )
+                    {
+                        ImgData.map((e, key) => {
+                            return (
+                                <>
+                                    <motion.div
+                                        whileDrag={{ scale: 1.1 }}
+
+                                        className="Skill_Card" key={key} >
+
+                                        <Image fill={true} quality={100} src={e.IMg} alt="skill img" />
+                                        <div className='overLay'><h1>{e.title}</h1></div>
+
+                                    </motion.div>
+                                </>
+                            )
+                        })
+                    }
+                </div>
+
+
+
+            </div>
+        </>
+    )
 }
 
 export default SkillSection_1
